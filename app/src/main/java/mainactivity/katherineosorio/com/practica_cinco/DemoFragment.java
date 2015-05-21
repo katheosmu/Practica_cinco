abc095014ee35a5c9df0e9963ff8a5822b5da59a
katheosmu <katheosmu@gmail.com>
2015-05-21 06:57:51 -0500

    Initial commit

---
+++ app/src/main/java/mainactivity/katherineosorio/com/practica_cinco/DemoFragment.java
@@ -0,0 +1,20 @@
+package mainactivity.katherineosorio.com.practica_cinco;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DemoFragment extends Fragment {
+
+     @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_demo, container, false);
+    }
+
+}
